4c9d7fec7ef1a759fde7f7c9191390d9307beb70
psierrajs <psierra.js@gmail.com>
2023-08-22 12:04:09 +0200

    Adding frame rate control

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -21,8 +22,10 @@ class AlienInvasion:
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
         
 if __name__ == '--main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
+
