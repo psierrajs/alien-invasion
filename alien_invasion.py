140f29eb582d5bfedc5596b1c6fcf33a68b984e4
psierrajs <psierra.js@gmail.com>
2023-08-22 11:35:56 +0200

    Created Pygame Window

---
+++ alien_invasion.py
@@ -0,0 +1,11 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self
